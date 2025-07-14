4f8c19b761d767c8756b21d54cdf8e887847a222
jongmin11 <gkswhdals01@gmail.com>
2025-07-14 17:46:53 +0900

    스킬만들기 시작

---
+++ IsekaiTextRPG/Skill.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace IsekaiTextRPG
+{
+    internal class Skill
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int Damage { get; set; }
+        public string Description { get; set; }
+        public int ManaCost { get; set; }
+        public int Cooldown { get; set; }
+        public Skill(string name, string description, int manaCost, int cooldown,int id,int damage)
+        {
+            Name = name;
+            Description = description;
+            ManaCost = manaCost;
+            Cooldown = cooldown;
+            Id = id;
+            Damage = damage;
+        }
+        public void skillUse()
+        {
+            
+
+        }
+    }
+
+
+}
